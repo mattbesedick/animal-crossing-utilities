10cd7c14101302fe6d791e9946272205f5500f8e
mattbesedick <mattbezy@gmail.com>
2020-07-11 14:07:03 -0500

    retreiving all documents for fish from  firebase, working on some other REST routes for creating/updating fish

--- server/api/fish.js
+++ server/api/fish.js
@@ -2,8 +2,32 @@ const router = require('express').Router()
 const db = require('../db')
 module.exports = router
 
+
+//route for adding/deleting fish into db
+
+//get all fish
 router.get("/", async (req, res, next) => {
-    res.send("this is fish")
+    try {
+
+        //make reference to collection
+        const docRef = db.collection('fish')
+
+        //get all fish
+        const result = await docRef.get()
+        const fish = []
+        //for each document returned, push to fish array
+        result.forEach((doc) => {
+            fish.push(doc.data())
+        })
+        res.send(fish)
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get("/:name", async (req, res, next) => {
+    res.send(`${req.params.name}`)
 })
 
 router.post("/", async (req, res, next) => {
@@ -15,3 +39,5 @@ router.post("/", async (req, res, next) => {
 
     res.send(result)
 })
+
+
