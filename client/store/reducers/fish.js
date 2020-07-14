837cb52e247cc6e8a9b377a86336283956e92ed2
mattbesedick <mattbezy@gmail.com>
2020-07-14 17:27:10 -0500

    removed store, going to use react-firebase-hooks for data fetching

--- client/store/reducers/fish.js
+++
@@ -1,7 +0,0 @@
-const initialState = {}
-
-const fishReducer = (state = initialState, action) => {
-    return null
-}
-
-export default fishReducer
--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase'
+import * as firebase from 'firebase-admin'
 
 var firebaseConfig = {
     apiKey: "AIzaSyCdzvLhrMGhbIWSYsA6cApwvSt72IX3qyw",
@@ -7,8 +7,8 @@ var firebaseConfig = {
     projectId: "animal-crossing-utilities",
     storageBucket: "animal-crossing-utilities.appspot.com",
     messagingSenderId: "353357348810",
-    appId: "1:353357348810:web:cbaf47da2c1bf6f74dd933",
-    measurementId: "G-DXNJJZFQMT"
+    appId: "1:353357348810:web:0a16d5dc52da90344dd933",
+    measurementId: "G-DNNMLXH23Y"
 };
 
 firebase.initializeApp(firebaseConfig)
